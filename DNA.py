bec718ca09fc858425514b26af4f528cdbbf151c
Sheeraz Ali <hacker.ascool@gmail.com>
2019-06-28 04:40:06 +0530

    added dna code decoder
    added dna code decoder

---
+++ DNA.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+mappings = {
+    'AAA':'a',
+    'AAC':'b',
+    'AAG':'c',
+    'AAT':'d',
+    'ACA':'e',
+    'ACC':'f',
+    'ACG':'g',
+    'ACT':'h',
+    'AGA':'i',
+    'AGC':'j',
+    'AGG':'k',
+    'AGT':'l',
+    'ATA':'m',
+    'ATC':'n',
+    'ATG':'o',
+    'ATT':'p',
+    'CAA':'q',
+    'CAC':'r',
+    'CAG':'s',
+    'CAT':'t',
+    'CCA':'u',
+    'CCC':'v',
+    'CCG':'w',
+    'CCT':'x',
+    'CGA':'y',
+    'CGC':'z',
+    'CGG':'A',
+    'CGT':'B',
+    'CTA':'C',
+    'CTC':'D',
+    'CTG':'E',
+    'CTT':'F',
+    'GAA':'G',
+    'GAC':'H',
+    'GAG':'I',
+    'GAT':'J',
+    'GCA':'K',
+    'GCC':'L',
+    'GCG':'M',
+    'GCT':'N',
+    'GGA':'O',
+    'GGC':'P',
+    'GGG':'Q',
+    'GGT':'R',
+    'GTA':'S',
+    'GTC':'T',
+    'GTG':'U',
+    'GTT':'V',
+    'TAA':'W',
+    'TAC':'X',
+    'TAG':'Y',
+    'TAT':'Z',
+    'TCA':'1',
+    'TCC':'2',
+    'TCG':'3',
+    'TCT':'4',
+    'TGA':'5',
+    'TGC':'6',
+    'TGG':'7',
+    'TGT':'8',
+    'TTA':'9',
+    'TTC':'0',
+    'TTG':' ',
+    'TTT':'.'
+}
+
+flag = []
+c = open('gen.txt').read().strip()
+
+for x in range(0,len(c),3):
+    piece = c [x:x+3]
+    print(piece, mappings[piece])
+    flag.append(mappings[piece])
+print (''.join(flag))
